b0352ecbd79b4e8dab2670d7bf2bf299d4159640
Thiều Quang Tuấn <tuancr261@gmail.com>
2017-03-04 10:00:50 +0700

    HSV IMAGE

--- opencv_hsv_pixel/opencv_hsv_pixel.cpp
+++ opencv_hsv_pixel/opencv_hsv_pixel.cpp
@@ -3,12 +3,12 @@
 #include <vector>
 using namespace cv;
 using namespace std;
-void calChannel(const vector<int>& colorValue, int& countPixels)
+void calChannel(const vector<int>& colorValue, int& count)
 {
 	for (int i{}; i < colorValue.size(); i++)
 	{
-		cout << left << setw(6) << "bin " << setw(3) << i << ": " << setw(6) << colorValue[i] << "| ";
-		countPixels += colorValue[i];
+		cout << left << setw(6) << "Value" << setw(3) << i << ": " << setw(6) << colorValue[i] << "| ";
+		count += colorValue[i];
 		if (i % 8 == 0) cout << endl;
 	}
 	cout << endl;
@@ -26,30 +26,26 @@ int main(int argc, char *argv[])
 	Mat output_HSV_img;
 	cvtColor(input_RGB_img, output_HSV_img, COLOR_BGR2HSV);
 	//--------------------------------------------------------------------------------------------
-	int numBin{};
-	cout << "Nhap vao so luong bin: ";
-	cin >> numBin;
-	int binRange{ 256 / numBin };
 	vector<vector<Vec3b>> pixel(output_HSV_img.rows, vector<Vec3b>(output_HSV_img.cols));
-	vector<int> redChannel(numBin, 0);
-	vector<int> greenChannel(numBin, 0);
-	vector<int> blueChannel(numBin, 0);
-	int countPixels{};
+	vector<int> HChannel(181, 0); //  H' <- H/2 (0 <= H <= 360)
+	vector<int> SChannel(256, 0);
+	vector<int> VChannel(256, 0);
+	int count{};
 	for (int i{}; i < output_HSV_img.rows; i++)
 		for (int j{}; j < output_HSV_img.cols; j++)
 		{
 			pixel[i][j] = output_HSV_img.at<Vec3b>(i, j);
-			blueChannel[(unsigned int)(pixel[i][j][0] / binRange)]++;
-			greenChannel[(unsigned int)(pixel[i][j][1] / binRange)]++;
-			redChannel[(unsigned int)(pixel[i][j][2] / binRange)]++;
+			HChannel[(unsigned int)(pixel[i][j][0])]++;
+			SChannel[(unsigned int)(pixel[i][j][1])]++;
+			VChannel[(unsigned int)(pixel[i][j][2])]++;
 		}
-	/*cout << "Blue Channel: " << endl;
-	calChannel(blueChannel, countPixels);
-	cout << "Green Channel: " << endl;
-	calChannel(greenChannel, countPixels);
-	cout << "Red Channel" << endl;
-	calChannel(redChannel, countPixels);*/
-	cout << "Pixels: " << countPixels << " " << output_HSV_img.rows * output_HSV_img.cols << endl;
+	cout << "H Channel: " << endl;
+	calChannel(HChannel,count);
+	cout << "S Channel: " << endl;
+	calChannel(SChannel, count);
+	cout << "V Channel" << endl;
+	calChannel(VChannel, count);
+	cout << "Pixels: " << output_HSV_img.rows * output_HSV_img.cols << " = " << count/3 <<endl;
 	namedWindow(argv[1], WINDOW_AUTOSIZE);
 	imshow(argv[1], output_HSV_img);
 	waitKey();
